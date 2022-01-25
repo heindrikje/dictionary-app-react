0e4cf92d57a00a684b54f7795c65674cc3b19477
heindrikje <heindrikje.kuhs@gmail.com>
2022-01-25 11:35:11 +0100

    Added unique keys for loops

--- src/ApiResults.js
+++ src/ApiResults.js
@@ -6,22 +6,31 @@ export default function ApiResults(props) {
     return (
       <div className="ApiResults">
         <h3 className="keyword">{props.data.word}</h3>
-        {props.data.meanings.map(function (meaning, index) {
+        {props.data.meanings.map(function (meaning) {
           return (
             <div>
-              <h4 key={index} className="partOfSpeech">
+              <h4
+                key={Math.random().toString(36).substr(2, 9)}
+                className="partOfSpeech"
+              >
                 {meaning.partOfSpeech}
               </h4>
               <div className="definition">
                 {meaning.definitions.map(function (definition) {
-                  return <p key="index">{definition.definition}</p>;
+                  return (
+                    <p key={Math.random().toString(36).substr(2, 9)}>
+                      {definition.definition}
+                    </p>
+                  );
                 })}
               </div>
               <div className="example">
                 {meaning.definitions.map(function (definition) {
                   return (
                     <p>
-                      <em key="index">{definition.example}</em>
+                      <em key={Math.random().toString(36).substr(2, 9)}>
+                        {definition.example}
+                      </em>
                     </p>
                   );
                 })}
