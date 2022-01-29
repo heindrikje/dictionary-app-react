2643df6fc72e35aef883353bedeeb34c8ff541ac
heindrikje <heindrikje.kuhs@gmail.com>
2022-01-29 01:55:40 +0100

    Display phonetics

--- src/ApiResults.js
+++ src/ApiResults.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 import "./ApiResults.css";
 
 export default function ApiResults(props) {
-  console.log(props.data);
   if (props.data) {
     return (
       <div className="ApiResults">
         <h3 className="keyword">{props.data.word}</h3>
+        <Phonetics phoneticsData={props.data.phonetics} />
         {props.data.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
---
+++ src/Phonetics.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Phonetics(props) {
+  return (
+    <div className="Phonetics">
+      {props.phoneticsData.map(function (phonetic, index) {
+        return (
+          <div key={index}>
+            <a
+              href={phonetic.audio}
+              target="_blank"
+              rel="noreferrer"
+              className="audio"
+            >
+              Listen
+            </a>
+            <div className="phoneticText">{phonetic.text}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+// icon für audio nutzen, dann onClick event
+// mit HTML5 audio api oder react audio player
