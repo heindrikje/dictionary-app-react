ad87b71c5e8a86147700027a39f4dcb367bc824b
heindrikje <heindrikje.kuhs@gmail.com>
2022-01-29 01:21:52 +0100

    Display synonyms

--- src/ApiResults.js
+++ src/ApiResults.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Meaning from "./Meaning";
+import "./ApiResults.css";
 
 export default function ApiResults(props) {
   console.log(props.data);
@@ -6,35 +8,10 @@ export default function ApiResults(props) {
     return (
       <div className="ApiResults">
         <h3 className="keyword">{props.data.word}</h3>
-        {props.data.meanings.map(function (meaning) {
+        {props.data.meanings.map(function (meaning, index) {
           return (
-            <div>
-              <h4
-                key={Math.random().toString(36).substr(2, 9)}
-                className="partOfSpeech"
-              >
-                {meaning.partOfSpeech}
-              </h4>
-              <div className="definition">
-                {meaning.definitions.map(function (definition) {
-                  return (
-                    <p key={Math.random().toString(36).substr(2, 9)}>
-                      {definition.definition}
-                    </p>
-                  );
-                })}
-              </div>
-              <div className="example">
-                {meaning.definitions.map(function (definition) {
-                  return (
-                    <p>
-                      <em key={Math.random().toString(36).substr(2, 9)}>
-                        {definition.example}
-                      </em>
-                    </p>
-                  );
-                })}
-              </div>
+            <div key={index}>
+              <Meaning meaningData={meaning} />
             </div>
           );
         })}
---
+++ src/Meaning.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Synonyms from "./Synonyms";
+
+export default function Meaning(props) {
+  return (
+    <div className="Meaning">
+      <h4 className="partOfSpeech">{props.meaningData.partOfSpeech}</h4>
+      {props.meaningData.definitions.map(function (definition, index) {
+        return (
+          <div key="index">
+            <p className="definition">{definition.definition}</p>
+            <div className="example">{definition.example}</div>
+            <Synonyms synonymData={definition.synonyms} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
